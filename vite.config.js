d8ac985ab98a39c5b27cb39ac6399830cb64765d
ZCC-Dev01 <cybercitydev01@gmail.com>
2024-12-18 21:30:12 +0200

    allowed access for hosing and open functionality over the web.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.VITE_PORT) || 3007,
+    open: true, // Open browser on server start
+    host: true, // Listen on all local IPs
+  }
 })
